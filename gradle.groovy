6c5adbd9085894cc85a16d4a761a8b4ee702afb9
Gonzalo Muñoz Boisier <gmboiser@gmail.com>
2022-01-28 21:50:17 -0300

    Actualiza pipeline

--- gradle.groovy
+++ gradle.groovy
@@ -38,11 +38,10 @@ def call() {
         ]
     }
     stage('Paso 5: Descargar Nexus') {
-        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD 
-        "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
     stage('Paso 6: Levantar Artefacto Jar') {
-        sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+        sh 'nohup java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
     }
     stage('Paso 7: Testear Artefacto - Dormir(Esperar 20sg) ') {
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
--- maven.groovy
+++ maven.groovy
@@ -47,7 +47,7 @@ def call() {
         sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
     stage('Paso 8: Levantar Artefacto Jar') {
-        sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+        sh 'nohup java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
     }
     stage('Paso 9: Testear Artefacto - Dormir(Esperar 20sg) ') {
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
